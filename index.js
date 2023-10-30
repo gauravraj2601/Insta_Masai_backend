c1fdbfcc2004c4626c5a2ac1b622e43a101fd3a4
gauravraj2601 <gauravraj2601@gmail.com>
2023-10-30 16:43:29 +0530

    add

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const { connection } = require("./db")
 const { userRouter } = require("./routes/user.routes")
 const { postRouter } = require("./routes/post.routes")
 const { auth } = require("./middleware/auth.middleware")
-
+const cors= require("cors")
 
 const app= express()
 app.use(express.json())
+
+app.use(cors())
 app.use("/users",userRouter)
 app.use("/posts",auth,postRouter)
 app.get("/",(req,res)=>{
